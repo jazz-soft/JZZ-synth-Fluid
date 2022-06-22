ac822bbec7244d12ae62cc66cc2a82ea38da7beb
Sema <jazz-dev@jazz-soft.net>
2022-06-22 00:41:52 -0400

    Passing arguments

--- index.js
+++ index.js
@@ -20,53 +20,79 @@
 
   var _version = '0.0.0';
 
-  function Synth(port) {
-    this.fluid = require('child_process').spawn('fluidsynth');
-    this.fluid.on('error', function(err) { port._crash('Cannot spawn fluidsynth: ' + err.message); });
-    this.fluid.on('spawn', function() { port._resume(); });
-    this.quit = function() { this.fluid.stdin.write('quit'); this.fluid.stdin.end(); };
+  function Synth(port, args) {
+    console.log(args);
+    var self = this;
+    self.fluid = require('child_process').spawn(args.path);
+    self.fluid.on('error', function(err) { port._crash('Cannot spawn fluidsynth: ' + err.message); });
+    self.fluid.on('spawn', function() {
+      if (args.sf) self.fluid.stdin.write('load ' + args.sf + '\n');
+      port._resume();
+    });
+    self.quit = function() { self.fluid.stdin.write('quit\n'); self.fluid.stdin.end(); };
 
-    this.play = function(msg) {
+    self.play = function(msg) {
 console.log('Playing: ' + msg);
     };
   }
 
   var _synth = {};
   var _noname = [];
-  var _engine = {};
 
-  _engine._info = function(name) {
-    if (!name) name = 'JZZ.synth.Fluid';
-    return {
-      type: 'Web Audio',
-      name: name,
-      manufacturer: 'virtual',
-      version: _version
+  function engine(args) {
+    var _engine = {};
+    _engine._info = function(name) {
+      if (!name) name = 'JZZ.synth.Fluid';
+      return {
+        type: 'FluidSynth',
+        name: name,
+        manufacturer: 'FluidSynth',
+        version: _version
+      };
     };
-  };
+    _engine._openOut = function(port, name) {
+      var synth;
+      if (typeof name !== 'undefined') {
+        name = '' + name;
+        if (!_synth[name]) _synth[name] = new Synth(port, args);
+        synth = _synth[name];
+      }
+      else {
+        synth = new Synth(port, args);
+        _noname.push(synth);
+      }
+      port._info = _engine._info(name);
+      port._receive = function(msg) { synth.play(msg); };
+      port._close = function() { synth.quit(); };
+    };
+    return _engine;
+  }
 
-  _engine._openOut = function(port, name) {
-    var synth;
-    if (typeof name !== 'undefined') {
-      name = '' + name;
-      if (!_synth[name]) _synth[name] = new Synth(port);
-      synth = _synth[name];
+  function _args(a) {
+    var name, par;
+    if (a.length == 2) {
+      name = a[0];
+      par = a[1];
     }
-    else {
-      synth = new Synth(port);
-      _noname.push(synth);
+    else if (a.length == 1) {
+      if (typeof a[0] == 'string') name = a[0];
+      else par = a[0];
     }
-    port._info = _engine._info(name);
-    port._receive = function(msg) { synth.play(msg); };
-    port._close = function() { synth.quit(); };
+    var args = { path: 'fluidsynth', sf: undefined };
+    if (typeof par == 'object') {
+      for (var k in args) if (par[k]) args[k] = par[k];
+    }
+    return [name, args];
+  }
+
+  JZZ.synth.Fluid = function() {
+    var a = _args(arguments);
+    return JZZ.lib.openMidiOut(a[0], engine(a[1]));
   };
 
-  JZZ.synth.Fluid = function(name) {
-    return JZZ.lib.openMidiOut(name, _engine);
-  };
-
-  JZZ.synth.Fluid.register = function(name) {
-    JZZ.lib.registerMidiOut(name, _engine);
+  JZZ.synth.Fluid.register = function() {
+    var a = _args(arguments);
+    JZZ.lib.registerMidiOut(a[0], engine(a[1]));
   };
 
   JZZ.synth.Fluid.version = function() { return _version; };
--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 const JZZ = require('jzz');
 require('.')(JZZ);
 
-JZZ.synth.Fluid()
+if (process.argv.length != 4) {
+  console.log('Usage: node test.js </path/to/fluidsynth> </path/to/soundfont.sf2>');
+  process.exit();
+}
+
+JZZ.synth.Fluid({ path: process.argv[2], sf: process.argv[3] })
   .or(function() {
     console.log('Cannot open MIDI-Out!');
     console.log(this._err());
